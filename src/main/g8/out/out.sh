2a779149e8fde6218fcf57e7bf758dd018233c15
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2015-06-11 16:44:48 +0200

    add scripts to verbatim, tested

--- src/main/g8/out/out.sh
+++ src/main/g8/out/out.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -x
-pandoc --toc --template=./ohnosequences.report.tex ../paper.md --smart -s --biblatex --bibliography ../refs.bib -o paper.tex
-xelatex paper.tex
-biber paper
-xelatex paper.tex
+ME=`dirname $0`
+pandoc --toc --template=${ME}/ohnosequences.report.tex ${ME}/../paper.md --smart -s --biblatex --bibliography ${ME}/../refs.bib -o ${ME}/paper.tex
+xelatex ${ME}/paper.tex
+biber ${ME}/paper
+xelatex ${ME}/paper.tex
