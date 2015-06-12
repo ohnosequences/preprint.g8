aafa22db519a4563f5354d887cc5f2dca9087cd4
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2015-06-12 10:54:14 +0200

    add output folder and halt on error for xelatex

--- src/main/g8/out/out.sh
+++ src/main/g8/out/out.sh
@@ -2,6 +2,6 @@
 set -x
 ME=`dirname $0`
 pandoc --toc --template=${ME}/ohnosequences.report.tex ${ME}/../paper.md --smart -s --biblatex --bibliography ${ME}/../refs.bib -o ${ME}/paper.tex
-xelatex ${ME}/paper.tex
+xelatex -output-directory ${ME} -halt-on-error ${ME}/paper.tex
 biber ${ME}/paper
-xelatex ${ME}/paper.tex
+xelatex -output-directory ${ME} -halt-on-error ${ME}/paper.tex
